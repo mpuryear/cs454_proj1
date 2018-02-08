286b005377cb117035653d3a0d3de54d1b225bce
Mathew Puryear <mpuryear@blue.cs.sonoma.edu>
2018-02-08 10:44:06 -0800

    Added main file

---
+++ proj1.py
@@ -0,0 +1,18 @@
+
+def prob1():
+
+    return
+
+
+def prob2():
+'''
+eg delta func :
+for n = input: k=26147 on the provided paper
+delta(i, j):  // returns next state, i is current state, j is next input
+return (10*i + j)%n
+
+
+must use bfs for something
+'''
+    
+    return
